ed8fa22bfdd0fbf76f49e399bf9fe450e65595c0
Kaushik-0407 <kaushikbhowmick108@gmail.com>
2024-06-18 18:37:57 +0530

    done

--- src/App.js
+++ src/App.js
@@ -3,18 +3,18 @@ import { BrowserRouter, Routes, Route } from "react-router-dom";
 import { ToastContainer } from "react-toastify";
 import { AppProvider } from "./contextApi/context";
 import Login from "./pages/Login";
-import AdminLayout from "./Layout/AdminLayout";
-import Temp from "./screen/Temp";
+import AdminLayout from "./layout/AdminLayout";
 import AllAdminCreate from "./pages/AllAdminCreate";
 import Wallet from "./pages/Wallet";
 import CreateSubAdmin from "./pages/CreateSubAdmin";
 import SubAdminView from "./pages/SubAdminView";
 import ViewSubAdminPermission from "./pages/ViewSubAdminPermission";
-import HierarchyPageView from './components/HierarchyPageView';
+import HierarchyPageView from "./components/HierarchyPageView";
 import AccountLandingModal from "./profileAccount/AccountLandingModal";
 
 import AdminAccountStatement from "./pages/AdminAccountStatement";
 import AgentDelete from "./pages/AgentDelete";
+import WelcomePage from "./screen/WelcomePage";
 
 function App() {
   return (
@@ -36,16 +36,28 @@ function App() {
           <Routes>
             <Route index element={<Login />} />
             <Route path="/" element={<AdminLayout />}>
-              <Route path="welcome" element={<Temp />} />
+              <Route path="welcome" element={<WelcomePage />} />
               <Route path="allAdminCreate" element={<AllAdminCreate />} />
-              <Route path="/adminaccountstatement" element={<AdminAccountStatement/>} />
-              <Route path="/agentDelete" element={<AgentDelete/>} />
-              <Route path="hierarchyView/:userName" element={<HierarchyPageView />} />
-              <Route path="/account-landing/:userName" element={<AccountLandingModal />} />
+              <Route
+                path="/adminaccountstatement"
+                element={<AdminAccountStatement />}
+              />
+              <Route path="/agentDelete" element={<AgentDelete />} />
+              <Route
+                path="hierarchyView/:userName"
+                element={<HierarchyPageView />}
+              />
+              <Route
+                path="/account-landing/:userName"
+                element={<AccountLandingModal />}
+              />
               <Route path="wallet" element={<Wallet />} />
               <Route path="CreateSubAdmin" element={<CreateSubAdmin />} />
               <Route path="ViewAllSubAdmin" element={<SubAdminView />} />
-              <Route path="ViewSubAdminPermission/:id" element={<ViewSubAdminPermission />} />
+              <Route
+                path="ViewSubAdminPermission/:id"
+                element={<ViewSubAdminPermission />}
+              />
             </Route>
           </Routes>
         </BrowserRouter>
