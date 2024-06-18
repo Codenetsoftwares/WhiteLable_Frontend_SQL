652728a6b4c6f1fd345fa1bd4ad076414dd40377
Kaushik-0407 <kaushikbhowmick108@gmail.com>
2024-06-18 17:32:22 +0530

    fixing

--- src/pages/SubAdminView.js
+++ src/pages/SubAdminView.js
@@ -1,16 +1,16 @@
-import React, { useState, useEffect } from 'react';
-import Pagination from '../components/common/Pagination';
-import { Link } from 'react-router-dom';
-import { useAppContext } from '../contextApi/context';
-import { getAllSubAdminCreate } from '../Utils/service/apiService';
-import { permissionObj } from '../Utils/constant/permission';
-import { getAllSubAdminCreateState } from '../Utils/service/initiateState';
+import React, { useState, useEffect } from "react";
+import Pagination from "../components/common/Pagination";
+import { Link } from "react-router-dom";
+import { useAppContext } from "../contextApi/context";
+import { getAllSubAdminCreate } from "../Utils/service/apiService";
+import { permissionObj } from "../Utils/constant/permission";
+import { getAllSubAdminCreateState } from "../Utils/service/initiateState";
 
 const SubAdminView = () => {
   const [subAdminData, setSubAdminData] = useState(getAllSubAdminCreateState());
 
   const { store, dispatch } = useAppContext();
-    console.log('(=====>> store line 15)',store)
+  console.log("(=====>> store line 15)", store);
 
   const handleChange = (name, value) => {
     setSubAdminData((prevData) => ({
@@ -21,9 +21,15 @@ const SubAdminView = () => {
 
   useEffect(() => {
     if (store?.admin) {
-      permissionObj.allAdmin.includes(store?.admin?.roles[0].role) && getAll_SubAdmin_Create();
+      permissionObj.allAdmin.includes(store?.admin?.roles[0].role) &&
+        getAll_SubAdmin_Create();
     }
-  }, [store?.admin, subAdminData.currentPage, subAdminData.name, subAdminData.totalEntries]);
+  }, [
+    store?.admin,
+    subAdminData.currentPage,
+    subAdminData.name,
+    subAdminData.totalEntries,
+  ]);
 
   async function getAll_SubAdmin_Create() {
     const response = await getAllSubAdminCreate({
@@ -44,16 +50,21 @@ const SubAdminView = () => {
   }
 
   const handlePageChange = (page) => {
-    console.log('Changing to page:', page);
-    handleChange('currentPage', page);
+    console.log("Changing to page:", page);
+    handleChange("currentPage", page);
   };
 
-  let startIndex = Math.min((Number(subAdminData.currentPage) - 1) * Number(subAdminData.totalEntries) + 1);
-  console.log('startIndex', subAdminData.currentPage);
+  let startIndex = Math.min(
+    (Number(subAdminData.currentPage) - 1) * Number(subAdminData.totalEntries) +
+      1
+  );
+  console.log("startIndex", subAdminData.currentPage);
   let endIndex = Math.min(
     Number(subAdminData.currentPage) * Number(subAdminData.totalEntries),
-    Number(subAdminData.totalData),
+    Number(subAdminData.totalData)
   );
+
+  console.log("data", subAdminData.userList);
 
   return (
     <div className="main_content_iner ">
@@ -75,7 +86,9 @@ const SubAdminView = () => {
                       <select
                         className="form-select form-select-sm"
                         aria-label=".form-select-sm example"
-                        onChange={(e) => handleChange('totalEntries', e.target.value)}
+                        onChange={(e) =>
+                          handleChange("totalEntries", e.target.value)
+                        }
                       >
                         <option selected value="5">
                           Show 5 entries
@@ -88,106 +101,141 @@ const SubAdminView = () => {
                       </select>
                     </div>
 
-                    <div className="serach_field_2 ms-auto" style={{ marginLeft: '-10px' }}>
+                    <div
+                      className="serach_field_2 ms-auto"
+                      style={{ marginLeft: "-10px" }}
+                    >
                       <div className="search_inner">
                         <form Active="#">
                           <div className="search_field">
                             <input
                               value={subAdminData.name}
                               onChange={(e) => {
-                                handleChange('name', e.target.value);
+                                handleChange("name", e.target.value);
                               }}
                               type="text"
                               placeholder="Search content here..."
                             />
                           </div>
                           <button type="submit">
-                            {' '}
-                            <i className="ti-search"></i>{' '}
+                            {" "}
+                            <i className="ti-search"></i>{" "}
                           </button>
                         </form>
                       </div>
                     </div>
                   </div>
-                  <div className="QA_table mb_30" style={{ overflow: 'auto' }}>
+                  <div className="QA_table mb_30" style={{ overflow: "auto" }}>
                     {/* table-responsive */}
-                    <table className="table lms_table_active3 ">
-                      <thead
-                        style={{
-                          height: '10px',
-                          backgroundColor: '#006699',
-                          color: 'white',
-                          fontWeight: 'bold',
-                        }}
-                      >
-                        <tr>
-                          <th scope="col" style={{ fontWeight: 'bold', color: 'white' }}>
-                            Serial Number
-                          </th>
-                          <th scope="col" style={{ fontWeight: 'bold', color: 'white' }}>
-                            Name
-                          </th>
-                          <th scope="col" style={{ fontWeight: 'bold', color: 'white' }}>
-                            Detail
-                          </th>
-                          <th scope="col" style={{ fontWeight: 'bold', color: 'white' }}>
-                            Status
-                          </th>
-                          <th scope="col" style={{ fontWeight: 'bold', color: 'white' }}>
-                            Change Status
-                          </th>
-                        </tr>
-                      </thead>
-                      <tbody>
-                        {subAdminData?.userList &&
-                          subAdminData?.userList?.map((user, index) => (
-                            <tr key={user._id}>
-                              <th scope="row">{index + 1}</th>
-                              <td>{user.userName}</td>
-
-                              <td>
-                                <Link to={`/ViewSubAdminPermission/${user.adminId}`}>
-                                  <button className="btn btn-sm btn-success">Go To</button>
-                                </Link>
-                              </td>
-                              <td>
-                                <button
-                                  className="border border-1 w-75 text-center bg-success rounded-pill "
-                                  style={{ cursor: 'auto' }}
-                                >
-                                  {user.status}
-                                </button>
-                              </td>
-                              <td>
-                                <span className="mx-1">
-                                  <button
-                                    className={`btn border border-2 rounded ${
-                                      store?.admin?.roles[0].permission.some((role) => role === 'Status')
-                                        ? ''
-                                        : [
-                                              'superAdmin',
-                                              'WhiteLabel',
-                                              'HyperAgent',
-                                              'SuperAgent',
-                                              'MasterAgent',
-                                            ].includes(store?.admin?.roles[0].role)
-                                          ? ''
-                                          : 'disabled'
-                                    }`}
-                                    title="Setting"
-                                    type="button"
-                                    onClick={() => {
-                                      // handleprops(user);
-                                    }}
-                                  >
-                                    <i className="fa-thin fas fa-gear"></i>
-                                  </button>
-                                </span>
-                              </td>
+                    {subAdminData?.userList.length > 0 ? (
+                      <React.Fragment>
+                        <table className="table lms_table_active3 ">
+                          <thead
+                            style={{
+                              height: "10px",
+                              backgroundColor: "#006699",
+                              color: "white",
+                              fontWeight: "bold",
+                            }}
+                          >
+                            <tr>
+                              <th
+                                scope="col"
+                                style={{ fontWeight: "bold", color: "white" }}
+                              >
+                                Serial Number
+                              </th>
+                              <th
+                                scope="col"
+                                style={{ fontWeight: "bold", color: "white" }}
+                              >
+                                Name
+                              </th>
+                              <th
+                                scope="col"
+                                style={{ fontWeight: "bold", color: "white" }}
+                              >
+                                Detail
+                              </th>
+                              <th
+                                scope="col"
+                                style={{ fontWeight: "bold", color: "white" }}
+                              >
+                                Status
+                              </th>
+                              <th
+                                scope="col"
+                                style={{ fontWeight: "bold", color: "white" }}
+                              >
+                                Change Status
+                              </th>
                             </tr>
-                          ))}
-                      </tbody>
-                    </table>
+                          </thead>
+                          <tbody>
+                            {subAdminData?.userList?.map((user, index) => (
+                              <tr key={user._id}>
+                                <th scope="row">{index + 1}</th>
+                                <td>{user.userName}</td>
+
+                                <td>
+                                  <Link
+                                    to={`/ViewSubAdminPermission/${user.adminId}`}
+                                  >
+                                    <button className="btn btn-sm btn-success">
+                                      Go To
+                                    </button>
+                                  </Link>
+                                </td>
+                                <td>
+                                  <button
+                                    className="border border-1 w-75 text-center bg-success rounded-pill "
+                                    style={{ cursor: "auto" }}
+                                  >
+                                    {user.status}
+                                  </button>
+                                </td>
+                                <td>
+                                  <span className="mx-1">
+                                    <button
+                                      className={`btn border border-2 rounded ${
+                                        store?.admin?.roles[0].permission.some(
+                                          (role) => role === "Status"
+                                        )
+                                          ? ""
+                                          : [
+                                              "superAdmin",
+                                              "WhiteLabel",
+                                              "HyperAgent",
+                                              "SuperAgent",
+                                              "MasterAgent",
+                                            ].includes(
+                                              store?.admin?.roles[0].role
+                                            )
+                                          ? ""
+                                          : "disabled"
+                                      }`}
+                                      title="Setting"
+                                      type="button"
+                                      onClick={() => {
+                                        // handleprops(user);
+                                      }}
+                                    >
+                                      <i className="fa-thin fas fa-gear"></i>
+                                    </button>
+                                  </span>
+                                </td>
+                              </tr>
+                            ))}
+                          </tbody>
+                        </table>
+                      </React.Fragment>
+                    ) : (
+                      <div className="alert text-dark bg-light" role="alert">
+                        <div className="alert-text d-flex justify-content-center">
+                          <b> &#128680; No Data Found !! </b>
+                        </div>
+                      </div>
+                    )}
                   </div>
                 </div>
               </div>
--- src/profileAccount/AccountLandingModal.js
+++ src/profileAccount/AccountLandingModal.js
@@ -114,7 +114,6 @@ const AccountLandingModal = () => {
     componentToRender = (
       <AccountProfile
         props={profileView}
-
         // UserName={userId}
       />
     );
